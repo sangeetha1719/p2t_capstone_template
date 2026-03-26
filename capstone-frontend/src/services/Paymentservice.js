63496abbadbf6bb6ca07a20b9807ce36c18378ed
Sangeetha Prabu <Sangeetha.prabu.np@outlook.com>
2026-03-26 19:34:55 -0400

    updated changes in services

--- capstone-frontend/src/services/Paymentservice.js
+++ capstone-frontend/src/services/Paymentservice.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const baseUrl = "http://localhost:5000";
+
+const baseUrl = import.meta.env.VITE_API_ENDPOINT;
 export async function checkoutCart(cartItems){
     const response = await axios.post(`${baseUrl}/checkout-session`, { cartItems });
     return response.data;
